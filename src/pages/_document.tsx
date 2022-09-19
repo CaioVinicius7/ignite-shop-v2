568e9eb9d08b743dbe69b56eec6df369c6876f45
Caio Vinícius <caio1525pereira@gmail.com>
2022-09-19 17:15:34 -0300

    feat: :tada: Base do projeto
    Configurações iniciais do projeto: stitiches, fontes, eslint, prettier, etc...

---
+++ src/pages/_document.tsx
@@ -0,0 +1,34 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+import { getCssText } from "../styles";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+
+        <link rel="shortcut icon" href="favicon.png" type="image/png" />
+        <meta name="description" content="Ignite Shop V2" />
+
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title> Home | Ignite Shop </title>
+      </Head>
+      <h1> Home </h1>
+    </>
+  );
+};
+
+export default Home;
